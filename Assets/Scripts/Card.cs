616bc2f5f906b7fa11698081b2c5065ef099ebdf
Digi-tex <101381059+Digi-tex@users.noreply.github.com>
2023-04-06 23:02:04 -0600

    Updated comments for the card script and reformatted code

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,33 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
-//using UnityEditor.SearchService;
 using UnityEngine;
-
-
 
 public class Card : MonoBehaviour
 {
-    public GameObject mainCamera;
+    public GameObject mainCamera;   //A reference to the main camera that holds the game manager script so that each card can modify the game params
 
     public int suit;                //Club 0 , diamond 1, heart 2, spade 3
     public int value;               //Value ace 1, 2-10, jack 11, queen 12, king 13
     public Sprite spriteFace;       //Sprite to hold the card face
     public Sprite spriteBack;       //Sprite to hold the card back
-    public bool clicked = false;
-    public bool inPyramid = false;
+    public bool inPyramid = false;  //A flag to determine if we should check collision
 
-    public ContactFilter2D colFilter;
+    public ContactFilter2D colFilter;                       //Used to handle collisions (filter not actually used, but needed for method)
+    public Collider2D[] colResults = new Collider2D[100];   //Used to handle collisions (100 is far too many array spots, but meh)
 
-    public Collider2D[] colResults = new Collider2D[100];
+    private Vector3 vel;            //Hold the returned speed of the movement
 
-    private Vector3 vel;
 
+
+    //Move an card from its current location to a new location
     public void MoveObject(Vector3 location)
     {
+        //Stop all existing coroutines on thsi card and start a new one
         StopAllCoroutines();
         StartCoroutine(SmoothMove(location));
     }
 
+
+
+    //Move the card smoothly from one spot to another using smooth damp
+    //Allow for some sloppiness in the movement so that a card isn't trying to be pixel perfect
     private IEnumerator SmoothMove(Vector3 end)
     {
         while (Mathf.Abs(this.transform.position.x - end.x) > 0.01 || Mathf.Abs(this.transform.position.y - end.y) > 0.01)
@@ -38,6 +41,9 @@ public class Card : MonoBehaviour
         yield return null;
     }
 
+
+
+    //Flip the card face from face-up to face-down or vice versa
     public void FlipCard()
     {
         if (this.GetComponent<SpriteRenderer>().sprite.name != this.GetComponent<Card>().spriteFace.name)
@@ -50,6 +56,10 @@ public class Card : MonoBehaviour
         }
     }
 
+
+
+    //If a card has been clicked, add it to the clicked cards pile of the game manager script
+    //Only do so if the card is face-up, meaning that it's in play
     private void OnMouseDown()
     {
         if (this.GetComponent<SpriteRenderer>().sprite == spriteFace)
@@ -63,10 +73,15 @@ public class Card : MonoBehaviour
         }
     }
 
+
+    //Check the collision on the card to determine if it should be face-up or face-down in the pyramid
+    //Each card will check its own collision and automatically update
     public void CheckCollision()
     {
+        //If the card is in the pyramid (board pile)
         if(inPyramid) 
         { 
+            //If the card is covered by at least two other cards (or is on the edge of the pyramid by colliding with an invisible gameobject)
             if (this.GetComponent<Collider2D>().OverlapCollider(colFilter, colResults) > 2)
             {
                 this.GetComponent<SpriteRenderer>().sprite = spriteBack;
@@ -80,12 +95,13 @@ public class Card : MonoBehaviour
 
     }
 
+    //Initialize the contact filter (could just be initialized above but w/e)
     void Start()
     {
         colFilter = new ContactFilter2D();
     }
 
-    // Update is called once per frame
+    //check collision every frame (kind of wasteful but w/e)
     void Update()
     {
         CheckCollision();
